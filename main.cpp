02731b2edb30b4bcc2f4374e3b9bc6048d6647a0
Zecyel <zecyel@163.com>
2024-04-12 15:01:34 +0800

    feat: include json package.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <fmt/color.h>
 #include "spdlog/spdlog.h"
+#include <nlohmann/json.hpp>
+
 
 int main() {
     fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold, "Hello, {}!\n", "fmt");
 
     spdlog::info("Hello, spdlog");
+
+    nlohmann::json a = {
+        { "key", "value" }
+    };
+    
     return 0;
 }
